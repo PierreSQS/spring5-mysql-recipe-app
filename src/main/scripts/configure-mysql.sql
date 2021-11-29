942b564c0ec874d548cd1856b110766e8f141e11
PierreSQS <nomo_messina@web.de>
2021-11-29 18:16:35 +0100

    Sec18-Chap308:added mysql configuration script

---
+++ src/main/scripts/configure-mysql.sql
@@ -0,0 +1,38 @@
+## Use to run mysql db docker image, optional if you're not using a local mysqldb
+# docker run --name mysqldb -p 3306:3306 -e MYSQL_ALLOW_EMPTY_PASSWORD=yes -d mysql
+
+# connect to mysql and run as root user
+# Create Databases
+create database sfg_dev;
+create database sfg_prod;
+
+# Create database service accounts
+create user 'sfg_dev_user'@'localhost' identified by 'sfg_dev_user';
+create user 'sfg_prod_user'@'localhost' identified by 'sfg_prod_user';
+create user 'sfg_dev_user'@'%' identified by 'sfg_dev_user';
+create user 'sfg_prod_user'@'%' identified by 'sfg_prod_user';
+
+# Database Grants
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'localhost';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'localhost';
+GRANT SELECT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT INSERT ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT DELETE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT UPDATE ON sfg_dev.* to 'sfg_dev_user'@'%';
+GRANT SELECT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT INSERT ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT DELETE ON sfg_prod.* to 'sfg_prod_user'@'%';
+GRANT UPDATE ON sfg_prod.* to 'sfg_prod_user'@'%';
+
+
+
+
+ 
+
+
